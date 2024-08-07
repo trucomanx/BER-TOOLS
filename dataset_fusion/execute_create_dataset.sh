c07c32a061505e61f6257c2662cde2a17fa2f1a5
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-08-07 18:01:22 -0300

    ordenando

--- dataset_fusion/execute_create_dataset.sh
+++ dataset_fusion/execute_create_dataset.sh
@@ -1,18 +1,25 @@
 
 
 InTrD="../../BER2024-SOURCE"
-DName="ber2024"
-OutDir="output"
+DName="ber2024-source"
+OutDir="../../BER2024/BER2024-FUSION"
+
+#strings=("train.csv" "test.csv")
+strings=("test.csv")
 
 ipynb-py-convert create_dataset.ipynb create_dataset.py
 
-python3 create_dataset.py   --model-type-body 'efficientnet_b3' \
-                            --model-type-face 'efficientnet_b3' \
-                            --model-type-skel 20 \
-                            --dataset-dir $InTrD \
-                            --dataset-file "train.csv" \
-                            --dataset-name $DName \
-                            --output-dir $OutDir
+for CsvFile in "${strings[@]}"; do
+    python3 create_dataset.py   --model-type-body 'efficientnet_b3' \
+                                --model-type-face 'efficientnet_b3' \
+                                --model-type-skel 20 \
+                                --dataset-dir $InTrD \
+                                --dataset-file $CsvFile \
+                                --dataset-name $DName \
+                                --output-dir $OutDir
+done
+
+
 
 
 rm -f create_dataset.py
